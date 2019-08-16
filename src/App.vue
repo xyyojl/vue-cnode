e3af7ff119667e42f39c24e82c567bd31ac677a8
xyyojl <m13631897256@163.com>
2019-08-16 23:03:51 +0800

    实现回到顶部功能

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       <router-view name="sideBar"></router-view>
     </div>
     <Footer></Footer>
+    <div class="backtotop" v-if="isShow" @click="backTop">回到顶部</div>
   </div>
 </template>
 
@@ -20,29 +21,65 @@ export default {
     Header,
     PostList,
     Footer
+  },
+  data () {
+    return {
+      isShow: false,
+      scrollTop : 0
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.scrollToTop)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.scrollToTop)
+  },
+  methods:{
+    // 点击图片回到顶部方法，加计时器是为了过渡顺滑
+    backTop () {
+        const that = this
+        let timer = setInterval(() => {
+          let ispeed = Math.floor(-that.scrollTop / 5)
+          document.documentElement.scrollTop = document.body.scrollTop = that.scrollTop + ispeed
+          if (that.scrollTop === 0) {
+            clearInterval(timer)
+          }
+        }, 16)
+    },
+    // 为了计算距离顶部的高度，当高度大于60显示回顶部图标，小于60则隐藏
+    scrollToTop () {
+      const that = this
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      that.scrollTop = scrollTop
+      if (that.scrollTop > 195) {
+        that.isShow = true
+      } else {
+        that.isShow = false
+      }
+    }
   }
 }
 </script>
 
 <style>
-  /* body{
-    margin: 0;
-    background-color: #e1e1e1;
-  }
-  .clearfix:after {
-      display: block;
-      height: 0;
-      content: "";
-      clear: both
-  }
-  *{box-sizing: border-box;}
-  .fl{float: left;}
-  .fr{float: right;}  */
   .main{
     width: 90%;
     max-width: 1400px;
     min-width: 960px;
     margin: 15px auto;
     min-height: 600px;
+  }
+  .backtotop{
+    position: fixed;
+    top: 345px;
+    right: 0px;
+    width: 24px;
+    color: gray;
+    padding: 12px 0 12px 5px;
+    cursor: pointer;
+    z-index: 2;
+    background: #fff;
+    border-radius: 12px 0 0 12px;
+    font-size: 14px;
   }
 </style>
